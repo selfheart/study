c92bff7d10b7796905881ba6bd07469b1c1be4b7
jiangzhuangzhuang <982136175@qq.com>
2023-07-12 20:24:34 +0800

    Automatically commit code

--- push_to_remote.bat
+++ push_to_remote.bat
@@ -3,7 +3,7 @@ cd /d "C:\Users\jiangzhuangzhuang\study\study"   REM 替换为您的代码仓库
 
         REM 执行Git命令向远程仓库推送代码
         git add .
-        git commit -m "自动推送代码"
+        git commit -m "Automatically commit code"
         git push origin master
     
 
