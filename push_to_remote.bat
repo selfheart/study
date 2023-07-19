d51e748511b8a892fa3180f70af052e94a3f1b5c
jiangzhuangzhuang <982136175@qq.com>
2023-07-19 16:21:36 +0800

    Automatically commit code

--- push_to_remote.bat
+++ push_to_remote.bat
@@ -1,15 +1,15 @@
 @echo off
-cd /d "C:\Users\jiangzhuangzhuang\study\study"   REM 替换为您的代码仓库路径
+cd /d "C:\Users\jiangzhuangzhuang\study\study"   
 
-REM 执行Git命令向远程仓库推送代码
+
 git add .
 git commit -m "Automatically commit code"
 git push origin master
 
 
-cd /d "C:\Users\jiangzhuangzhuang\study\JavaWork\javastudy"   REM 替换为您的代码仓库路径
+cd /d "C:\Users\jiangzhuangzhuang\study\JavaWork\javastudy"   
 
-REM 执行Git命令向远程仓库推送代码
+
 git add .
 git commit -m "Automatically commit code"
 git push origin master
